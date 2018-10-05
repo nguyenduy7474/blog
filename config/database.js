0ef5c4a354685ab0dd82e0f67742f36502519d18
nguyenduy7474 <nguyenduy7474@gmail.com>
2018-10-05 12:50:07 +0700

    connect database mongodb

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost:27017/blogrieng' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://nguyenduy7474:duy0989618769@ds223653.mlab.com:23653/blogcuaduy' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
      
      //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
 };
